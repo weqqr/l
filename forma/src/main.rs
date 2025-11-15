0b667c544670d8fcac50b42100b47da6d7a4315c
weqqr <weqqr@yandex.ru>
2025-11-16 00:46:10 +0300

    Add application stub for Forma

--- forma/src/main.rs
+++ forma/src/main.rs
@@ -1,3 +1,56 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+
+use winit::application::ApplicationHandler;
+use winit::dpi::PhysicalSize;
+use winit::event::WindowEvent;
+use winit::event_loop::{ActiveEventLoop, ControlFlow, EventLoop};
+use winit::window::{Window, WindowId};
+
+struct App {
+    window: Option<Window>
+}
+
+impl App {
+    pub fn new() -> Self {
+        Self {
+            window: None,
+        }
+    }
+}
+
+impl ApplicationHandler for App {
+    fn resumed(&mut self, event_loop: &ActiveEventLoop) {
+        let window_attributes = Window::default_attributes()
+            .with_title("Forma")
+            .with_inner_size(PhysicalSize::new(1280, 720));
+
+        let window = event_loop.create_window(window_attributes).unwrap();
+
+        self.window = Some(window);
+    }
+
+    fn window_event(
+        &mut self,
+        event_loop: &ActiveEventLoop,
+        _window_id: WindowId,
+        event: WindowEvent,
+    ) {
+        event_loop.set_control_flow(ControlFlow::Wait);
+
+        match event {
+            WindowEvent::CloseRequested => event_loop.exit(),
+            _ => {}
+        }
+    }
+
+    fn about_to_wait(&mut self, event_loop: &ActiveEventLoop) {}
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let event_loop = EventLoop::new()?;
+    let mut app = App::new();
+
+    event_loop.run_app(&mut app)?;
+
+    Ok(())
 }
